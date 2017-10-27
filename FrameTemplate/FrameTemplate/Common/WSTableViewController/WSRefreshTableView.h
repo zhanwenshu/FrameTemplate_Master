f6e2296536852619cf0f3a51f57cd0d07a8f91b7
zws <zhanwenshu@sihemedia.com>
2017-10-27 16:35:20 +0800

    添加注释

--- FrameTemplate/FrameTemplate/Common/WSTableViewController/WSRefreshTableView.h
+++ FrameTemplate/FrameTemplate/Common/WSTableViewController/WSRefreshTableView.h
@@ -35,15 +35,68 @@ typedef NS_ENUM(NSInteger, RefreshCategory) {
 @property (nonatomic, assign) BOOL isLoadedAllTheData;
 @property (nonatomic, weak) id<WSRefreshTableViewDelegate> customTableDelegate;
 
+
+/**
+ 数据加载完成后调用此函数，结束刷新动作
+ */
 - (void)doneLoadingTableViewData;
+
+/**
+ 设置刷新模式，仅使用下拉、上下拉、下拉，或者不使用上下拉
+
+ @param refreshCategory 刷新模式
+ */
 - (void)setRefreshCategory:(RefreshCategory)refreshCategory;
+
 - (void)setLoadedAllTheData;
+
+
+/**
+ 无数据时使用，添加无数据背景
+
+ @param image 背景图片
+ @param tips 提示
+ */
 - (void)showNoDataViewWithImage:(UIImage *)image tips:(NSString *)tips;
+
+
+/**
+ 重新设置无数据frame
+
+ @param frame 要设定的frame
+ */
 - (void)resetNoDataViewFrame:(CGRect)frame;
+
+
+/**
+ 隐藏无数据背景
+ */
 - (void)hideNoDataView;
+
+
+/**
+ 自定义背景
+
+ @param view 自定义的view
+ */
 - (void)showCustomView:(UIView*)view;
+
+
+/**
+ 隐藏自定义背景
+ */
 - (void)hideCustomView;
+
+
+/**
+ 开始刷新
+ */
 - (void)beginRefreshing;
+
+
+/**
+ 刷新后列表的动画
+ */
 - (void)reloadDataWithAnimate;
 
 @end
--- FrameTemplate/FrameTemplate/Common/WSTableViewController/WSTableViewController.h
+++ FrameTemplate/FrameTemplate/Common/WSTableViewController/WSTableViewController.h
@@ -9,6 +9,11 @@
 #import <UIKit/UIKit.h>
 #import "WSRefreshTableView.h"
 
+
+/**
+ 继承WSTableViewController，享有tableview控件，可使用xib初始化tableview
+ 子类拥有上下拉刷新功能，可自行设定
+ */
 @interface WSTableViewController : UIViewController
 
 @property (strong, nonatomic) IBOutlet WSRefreshTableView *tableView;
